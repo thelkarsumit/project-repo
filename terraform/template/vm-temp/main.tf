a6b8688703b73da192ea396fb04e247c286f8bf6
Sumit Thelkar <thelkar.sumit@hcltech.com>
2024-12-20 12:40:48 +0000

    adding all files

--- terraform/template/vm-temp/main.tf
+++ terraform/template/vm-temp/main.tf
@@ -1,9 +1,9 @@
 module "vm" {
-  source = "../../vm"
+  source = "../../modules/vm"
   vm_name = var.vm_name
-  mahine_type = var.machine_type
+  machine_type = var.machine_type
   zone = var.zone
-  project = var.project_id
+  project_id = var.project_id
   image = var.image
   network = var.network
   subnetwork = var.subnetwork
