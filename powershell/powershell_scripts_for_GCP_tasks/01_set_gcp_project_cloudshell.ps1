b836777574bec73f14674df478f9e3ece2d80550
Sumit Thelkar <thelkar.sumit@hcltech.com>
2025-01-17 03:43:10 +0000

    adding list project commad

--- powershell/powershell_scripts_for_GCP_tasks/01_set_gcp_project_cloudshell.ps1
+++ powershell/powershell_scripts_for_GCP_tasks/01_set_gcp_project_cloudshell.ps1
@@ -5,3 +5,7 @@ $projectID = "shyamkprj"
 # Set the project as the active project for future operations
 gcloud config set project $projectID
 Write-Host "Project $projectName is now active."
+
+$projects = gcloud projects list --format="table(projectId, name, projectNumber, lifecycleState)"
+Write-Host "Google Project"
+Write-Output $projects
